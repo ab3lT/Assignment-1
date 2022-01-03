6217d2a7cc243a90be8db2a961326145f2d5274e
AB3L <se.abel.tadesse@gmail.com>
2022-01-03 08:04:31 -0800

    Final reverse string

--- Program.cs
+++ Program.cs
@@ -6,22 +6,17 @@ namespace assignment_one
     {
         static void Main(string[] args)
         {
-            string input;
             string revString = "";
+            string input;
             Console.Write("Enter string : ");
             input = Console.ReadLine();
-            /* input = input.ToLower(); */
+            
             for(int i=input.Length-1; i>=0; i--){
                 revString = revString + input[i];
             }
             
             Console.WriteLine($"Reverse String is : {revString}");
-            /* if(input==revString)
-            {
-                Console.WriteLine("\nString is palindrome");
-            }
-            else
-                Console.WriteLine("\nString not palindrome."); */
+            
          
             Console.WriteLine("The program excuted perfectly!!");
         }

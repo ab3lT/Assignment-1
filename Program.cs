98d21c90a815bef56acd57e6399f4803cc2919a2
AB3L <se.abel.tadesse@gmail.com>
2022-01-03 07:51:12 -0800

    reversing a sting

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace assignment_one
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            string revString = "";
+            Console.Write("Enter string : ");
+            input = Console.ReadLine();
+            /* input = input.ToLower(); */
+            for(int i=input.Length-1; i>=0; i--){
+                revString = revString + input[i];
+            }
+            
+            Console.WriteLine($"Reverse String is : {revString}");
+            /* if(input==revString)
+            {
+                Console.WriteLine("\nString is palindrome");
+            }
+            else
+                Console.WriteLine("\nString not palindrome."); */
+         
+            Console.WriteLine("The program excuted perfectly!!");
         }
     }
 }

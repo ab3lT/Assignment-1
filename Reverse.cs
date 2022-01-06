36f1e85be191b1254b21e062dad30178c5a59c8b
AB3L <se.abel.tadesse@gmail.com>
2022-01-06 11:19:39 -0800

    adding menu

--- Reverse.cs
+++ Reverse.cs
@@ -4,6 +4,7 @@ namespace assignment_one
 {
     class Reverse{
         public static void reverse(string input){
+            
             string revString = "";
             for(int i=input.Length-1; i>=0; i--){
                 revString = revString + input[i];

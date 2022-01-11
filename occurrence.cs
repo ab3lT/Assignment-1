e2cfeb584742b229925428406249594ed9fb6fb1
AB3L <se.abel.tadesse@gmail.com>
2022-01-11 06:52:58 -0800

    replacing while space with empity string

--- occurrence.cs
+++ occurrence.cs
@@ -7,13 +7,14 @@ namespace assignment_one
         public static void Occurrence(string str)
         {
             Console.WriteLine("String: " + str);
+            str = str.Replace(" ", string.Empty);
             while (str.Length > 0)
             {
                 Console.Write(str[0] + " => ");
                 int coccurance = 0;
                 for (int i = 0; i < str.Length; i++)
                 {
-                    if (str[0] == str[i])
+                    if (str[0] != str[i] || str[0] == str[i])
                     {
                         coccurance++;
                     }
